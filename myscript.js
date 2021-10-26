d91ccbe4bc168ad2cfd62d10f869f81bf10a76ca
Lafoulks <foulks.lauren@gmail.com>
2021-10-25 20:07:29 -0400

    added conditions to playRound function

--- myscript.js
+++ myscript.js
@@ -4,13 +4,25 @@ function computerPlay(choices) {
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
+console.log(computerPlay(choices));
+
 const computerSelection = computerPlay(choices);
-const playerSelection = 'Rock';
+const playerSelection = prompt ('What will you choose?', 'Rock, Paper, or Scissors?');
 
 function playRound(playerSelection, computerSelection) {
-    if ((computerSelection = 'Rock') && (playerSelection = 'Rock') || 
-    (computerSelection = 'Scissors') && (playerSelection = 'Scissors') ||
-    (computerSelection = 'Paper') && (playerSelection = 'Paper')) {
-        alert('It\'s a tie!');
-    }      
+    if ((computerSelection == 'Rock' && playerSelection == 'Rock') || 
+        (computerSelection == 'Scissors' && playerSelection == 'Scissors') ||
+        (computerSelection == 'Paper' && playerSelection == 'Paper')) {
+            alert('It\'s a tie!');
+    }   else if ((computerSelection == 'Rock' && playerSelection == 'Scissors') || 
+        (computerSelection == 'Scissors' && playerSelection == 'Paper') ||
+        (computerSelection == 'Paper' && playerSelection == 'Rock')) {
+            alert('Oh no! You lost!');
+    }   else ((computerSelection == 'Rock' && playerSelection == 'Paper') || 
+        (computerSelection == 'Scissors' && playerSelection == 'Rock') ||
+        (computerSelection == 'Paper' && playerSelection == 'Scissors')); {
+            alert('You win!');
+    }
+    return (playRound(playerSelection, computerSelection));
 }
+    console.log(playRound(playerSelection, computerSelection));
